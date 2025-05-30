6344e94ee11b14514b907687060497561324f0aa
yairlara <yairlara1626@gmail.com>
2025-05-30 15:38:26 +0000

    matrices

--- trabajo1.cpp
+++ trabajo1.cpp
@@ -1,1 +1,25 @@
-#inclu
+#include <iostream>
+using namespace std;
+
+int main(){
+    int f;
+    int c;
+    cout<<"dime las filas"<<endl;
+    cin>>f;
+    cout<<"dime las columnas"<<endl;
+    cin>>c;
+    int matriz[f][c];
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
+        cout<<"ingresa la matriz en la posicion "<<i<<" "<<j<<endl;
+        cin>>matriz[i][j];
+        }
+        }
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
+            cout<<" "<<matriz[i][j]<<" ";
+                }
+            cout<<"\n";
+            }
+
+    }
